02085358c58e6ab66d09e7507a3b3259a22c33db
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-06-12 10:38:28 +0400

    feat: added cases responsive

--- app/components/OurCases/OurCases.tsx
+++ app/components/OurCases/OurCases.tsx
@@ -30,6 +30,9 @@ const OurCases = () => {
                         <p>{t('cases.0.description')}</p>
                     </div>
                 </div>
+
+                <h3 className={styles.leptopH3}>{t('cases.0.title')}</h3>
+
                 <div className={styles.secondSectionWrapper}>
                     <div className={styles.avseLanding}>
                         <div className={styles.secondaryTexts}>
@@ -43,6 +46,9 @@ const OurCases = () => {
                             width={481}
                         />
                     </div>
+
+                    <h3 className={styles.leptopH3}>{t('cases.1.title')}</h3>
+
                     <div className={styles.pilzWrapper}>
                         <div className={styles.secondaryTexts}>
                             <h3>{t('cases.2.title')}</h3>
@@ -53,8 +59,18 @@ const OurCases = () => {
                             alt='pilzLanding'
                             height={307}
                             width={481}
+                            className={styles.pilzLandingImg}
+                        />
+                        <Image
+                            src={'/pilzLandingLaptop.png'}
+                            alt='pilzLanding'
+                            className={styles.pilzLandingLaptopImg}
+                            height={307}
+                            width={381}
                         />
                     </div>
+                    <h3 className={styles.leptopH3}>{t('cases.2.title')}</h3>
+
                 </div>
             </div>
         </div>
