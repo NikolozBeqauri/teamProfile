7818fe7161afbdb4ca7cf5ba82887a15eeef2ac0
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2025-06-15 23:38:54 +0400

    feat: added keyWords

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -30,18 +30,23 @@ export async function generateMetadata({
 
   const keywords: { [key: string]: string[] } = {
     en: [
-      'web development', 'mobile app development', 'UI/UX design', 'software company',
-      'digital solutions', 'custom software', 'IT consulting', 'GITA certified',
-      'React', 'Next.js', 'mobile apps', 'web apps', 'software development agency',
-      'Synaptica', 'frontend', 'backend', 'tech company Georgia', 'digital services',
-      'startup support', 'SaaS development', 'mobile UI', 'web UI'
+      'Web Development', 'Mobile App Development', 'UI/UX Design', 'Software Company', 'Digital Solutions', 'Software',
+      'React', 'Next.js', 'Mobile Application', 'Website', 'Development Agency', 'Synaptica',
+      'Frontend', 'Backend', 'Tech Company Georgia', 'Digital Services',
+      'Startup Support', 'Mobile Design', 'Web Design', 'Website Creation', 'App Creation',
+      'Affordable Website Creation', 'Affordable App Development', 'Website Development', 'Responsive Website',
+      'Personal Website', 'Portfolio Creation', 'Portfolio', 'Business Website', 'React Developer',
+      'Next.js Developer', 'Frontend Developer'
     ],
     ka: [
       'ვებ დეველოპმენტი', 'მობილური აპლიკაციების დეველოპმენტი', 'UI/UX დიზაინი', 'სოფტვეერის კომპანია',
       'ციფრული გადაწყვეტილებები', 'პროგრამული უზრუნველყოფა',
-      'React', 'Next.js', 'მობილური აპლიკაცია', 'ვებსაიტი', 'დეველოპერული სააგენტო',
+      'React', 'Next.js', 'მობილური აპლიკაცია', 'ვებსაიტი', 'დეველოპერული სააგენტო', 'სინაფტიკა', 'სინაპტიკა',
       'Synaptica', 'ფრონტენდი', 'ბექენდი', 'ტექნოლოგიური კომპანია საქართველო', 'დიჯიტალური სერვისები',
-      'სტარტაპ მხარდაჭერა', 'SaaS განვითარება', 'მობილური დიზაინი', 'ვებ დიზაინი', 'საიტის აწყობა', 'აპლიკაციის აწყობა',
+      'სტარტაპ მხარდაჭერა', 'მობილური დიზაინი', 'ვებ დიზაინი', 'საიტის აწყობა', 'აპლიკაციის აწყობა',
+      'საიტის აწყობა იაფად', 'აპლიკაციის აწყობა იაფად', 'საიტის დამზადება', 'რესპონსიული ვებსაიტი',
+      'პერსონალური ვებსაიტი', 'პორტფოლიოს აწყობა', 'პორთფოლიო', 'ბიზნეს საიტი', 'React დეველოპერი',
+      'Next.js დეველოპერი', 'ფრონტენდ დეველოპერი'
     ]
   };
 
